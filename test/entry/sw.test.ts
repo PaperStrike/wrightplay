2e8af1b3f4054e089dc0b248896bf908bbae63ca
华 <sliphua@outlook.com>
2022-06-19 23:17:48 +0800

    chore: clean up after each sw test

--- test/entry/sw.test.ts
+++ test/entry/sw.test.ts
@@ -1,13 +1,23 @@
 import { it, expect } from '../default.setup.js';
 
 describe('service worker', () => {
-  it('should register', async () => {
-    const container = window.navigator.serviceWorker;
-    if (!(await container.register('./sw.js')).active) {
+  const registrations: ServiceWorkerRegistration[] = [];
+  const register = async (...args: Parameters<ServiceWorkerContainer['register']>) => {
+    const reg = await window.navigator.serviceWorker.register(...args);
+    registrations.push(reg);
+    if (!reg.active) {
       await new Promise((resolve) => {
-        container.addEventListener('controllerchange', resolve);
+        window.navigator.serviceWorker.addEventListener('controllerchange', resolve);
       });
     }
+  };
+
+  it.afterEach(async () => {
+    await Promise.all(registrations.map((reg) => reg.unregister()));
+  });
+
+  it('should register', async () => {
+    await register('./sw.js');
     await expect((await fetch('/sw-status')).text()).resolves.toBe('sw ready!');
   });
 });
