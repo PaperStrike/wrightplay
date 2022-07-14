64392ab0ed25eae8a8cfbeb260818b7166ec810f
华 <sliphua@outlook.com>
2022-07-15 00:48:41 +0800

    feat(route): switch to context route

--- test/route/basic.test.ts
+++ test/route/basic.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from '../default.setup.js';
 import {
-  pageRoute,
-  pageUnroute,
+  contextRoute,
+  contextUnroute,
   bypassFetch,
   Route,
 } from '../../src/index.js';
@@ -18,42 +18,42 @@ describe('basic routing', () => {
   });
 
   it('should route', async () => {
-    await pageRoute('/route', routeCallback);
+    await contextRoute('/route', routeCallback);
     await expect((await fetch('/route')).text()).resolves.toBe('routed');
     expect(float.length).toBe(1);
   });
 
   it('should route 1 time', async () => {
-    await pageRoute('/route-1', routeCallback, { times: 1 });
+    await contextRoute('/route-1', routeCallback, { times: 1 });
     await expect((await fetch('/route-1')).text()).resolves.toBe('routed');
     expect((await fetch('/route-1')).status).toBe(404);
     expect(float.length).toBe(1);
   });
 
   it('should unroute handler', async () => {
-    await pageRoute('/cancel-handler', (r) => {
+    await contextRoute('/cancel-handler', (r) => {
       float.push(r.fulfill({ body: 'fallback' }));
     });
-    await pageRoute('/cancel-handler', routeCallback);
+    await contextRoute('/cancel-handler', routeCallback);
     await expect((await fetch('/cancel-handler')).text()).resolves.toBe('routed');
-    await pageUnroute('/cancel-handler', routeCallback);
+    await contextUnroute('/cancel-handler', routeCallback);
     await expect((await fetch('/cancel-handler')).text()).resolves.toBe('fallback');
     expect(float.length).toBe(2);
   });
 
   it('should unroute all handlers for url', async () => {
-    await pageRoute('/cancel-url', (r) => {
+    await contextRoute('/cancel-url', (r) => {
       float.push(r.fulfill({ body: 'fallback' }));
     });
-    await pageRoute('/cancel-url', routeCallback);
+    await contextRoute('/cancel-url', routeCallback);
     await expect((await fetch('/cancel-url')).text()).resolves.toBe('routed');
-    await pageUnroute('/cancel-url');
+    await contextUnroute('/cancel-url');
     expect((await bypassFetch('/cancel-url')).status).toBe(404);
     expect(float.length).toBe(1);
   });
 
   it('should bypass route', async () => {
-    await pageRoute('/bypass-test', routeCallback);
+    await contextRoute('/bypass-test', routeCallback);
     expect((await fetch('/bypass-test')).status).toBe(200);
     expect((await bypassFetch('/bypass-test')).status).toBe(404);
     expect(float.length).toBe(1);
